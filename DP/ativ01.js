5fa105e1b6a5cfc9f1e3bec1384bd701d1bc00c0
Edy_PJs <141849165+edy3333@users.noreply.github.com>
2024-09-24 12:34:08 -0400

    Add files via upload

---
+++ DP/ativ01.js
@@ -0,0 +1,12 @@
+//Adicionar nomes
+let alunos = ["Maria","João","Ana","Pedro"]
+console.log(alunos);
+//Adicionar o nome Carlos ao final da lista de alunos
+alunos.push("Carlos")
+console.log(alunos);
+//Remover um nome da lista de aluno
+alunos.shift()
+console.log(alunos);
+//Ordenar a lista por ordem alfabética
+alunos.sort()
+console.log(alunos);
---
+++ DP/ativ02.js
@@ -0,0 +1,9 @@
+//MATRIZES
+var alunos = ["Maria","João","Ana","Pedro"];
+console.log(alunos);
+console.log(alunos[2]);  // imprimi array desejada 
+console.log(alunos.length);  // tamanho de array
+console.table(alunos); //  exibição de tabela
+// e se eu quero trocar joao para adriana , como eu faria ?
+alunos[1]='Adriana';
+console.table(alunos);
